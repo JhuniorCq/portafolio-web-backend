a77d0e5de14c9baa44c1f6f2f8bd3106aa0c9236
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2025-01-20 23:20:57 -0500

    .

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://portafolio-jhunior-ccora.vercel.app/",
+      "https://portafolio-jhunior-ccora.vercel.app",
     ],
   })
 );
